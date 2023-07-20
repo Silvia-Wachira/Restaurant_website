33f6fb67a52ee5fdfd97c500ca4b9126f3b9313b
Silvia-Wachira <wachirasilvia8@gmail.com>
2023-07-20 16:06:01 +0300

    build a redirecct function

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
 
-//Create  a card
-document.querySelector('#reserve-btn').addEventListener
-('click', buildCard)
+// Create  a card
+// document.querySelector('#reserve-btn').addEventListener
+// ('click', buildCard)
 
-function buildCard(){
-   let card = document.createElement('div ')
-   card.className = 'reservations-card'
-   card.innerHTML = `
-    <form class="reservation-form" >
-        <label for="name">Name</label>
-        <input type="text" name="Name" placeholder="Enter your name" > 
-        <label for="email">Email</label>
-        <input type="email" placeholder="Enter your email" >
-        <button>Find a Table</button>
-    </form>
+// function buildCard(){
     
-   `
+//    let card = document.createElement('card')
+//    card.className = 'reservations-card'
+//    card.textContent = 'reserv'
+//    console.log(reserv)
+//    card.innerHTML = `
+//     <form class="reservation-form" >
+//         <label for="name">Name</label>
+//         <input type="text" name="Name" placeholder="Enter your name" > 
+//         <label for="email">Email</label>
+//         <input type="email" placeholder="Enter your email" >
+//         <button>Find a Table</button>
+//     </form>
+//    `
 
-}
+// }
 
