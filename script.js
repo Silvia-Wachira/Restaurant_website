5495c655d2e83ede1601c48e0b39d216d7d29f19
Silvia-Wachira <wachirasilvia8@gmail.com>
2023-07-20 13:31:28 +0300

    build a dom element card

--- script.js
+++ script.js
@@ -4,5 +4,18 @@ document.querySelector('#reserve-btn').addEventListener
 ('click', buildCard)
 
 function buildCard(){
-   return alert('I am a card')
+   let card = document.createElement('div ')
+   card.className = 'reservations-card'
+   card.innerHTML = `
+    <form class="reservation-form" >
+        <label for="name">Name</label>
+        <input type="text" name="Name" placeholder="Enter your name" > 
+        <label for="email">Email</label>
+        <input type="email" placeholder="Enter your email" >
+        <button>Find a Table</button>
+    </form>
+    
+   `
+
 }
+
